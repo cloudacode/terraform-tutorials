c43944520486f87f0720f6dafa97d0b27ab98bea
kcfigaro <kcfigaro@gmail.com>
2021-10-01 22:14:02 +0200

    updated the output values

--- 03-module/output.tf
+++ 03-module/output.tf
@@ -6,11 +6,9 @@ output "ec2_instance_public_ips" {
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = module.db_service.rds_hostname
-  sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = module.db_service.rds_port
-  sensitive   = true
 }
